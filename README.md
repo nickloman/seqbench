9ec150df3ff05cf5201f02bc7e5c34c08bc77dc2
Nick Loman <n.j.loman@bham.ac.uk>
2013-02-19 07:50:16 -0500

    seqbench init checkin

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-seqbench
-========
+
+Seqbench.
+
+	reads/   - metadata and links to sequence reads, arranged by:
+		<reference_name>/<source>/<run_id>.yaml
+		e.g. 
+		Ecoli_K12/University_of_Birmingham/IonTorrent-316-1.yaml
+
+	recipes/ - standard recipes for performing regular tasks
+
+	analysis/ - results of analysis
+
+	website/ - the source-code to drive the website Seqbench.org
+
+
+	
+
---
+++ reads/seqbench.sql
@@ -0,0 +1,23 @@
+drop table runs;
+create table runs (
+	Species	text,
+	Subspecies text,
+	Strain text,
+	Platform text,
+	Centre text,
+	Replicate text,
+	Planned_read_length text,
+	Chip text,
+	Mode text,
+	Fragment_size text,
+	Accession text,
+	URL text,
+	Reference text,
+	Isolation_Date text,
+	Country_of_Origin text,
+	x text,
+	Clinical_Details text,
+	SRA_SRP_Number text,
+	SRA_Experiment text,
+	SRA_Study text
+);
